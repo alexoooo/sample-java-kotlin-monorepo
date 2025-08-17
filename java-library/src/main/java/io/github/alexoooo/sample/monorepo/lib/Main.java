170b6feaff0fe39b5860bde01d77fbf6b0b9d01d
alexoooo <alex@ostrovsky.biz>
2025-08-17 15:32:15 -0400

    test commit

--- java-library/src/main/java/io/github/alexoooo/sample/monorepo/lib/Main.java
+++ java-library/src/main/java/io/github/alexoooo/sample/monorepo/lib/Main.java
@@ -2,6 +2,6 @@ package io.github.alexoooo.sample.monorepo.lib;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("foo");
     }
 }
